df2834ee9e6498c28dea0e939ef4f1a43e1500ce
ZEK-XCPY <zaid03297@gmail.com>
2025-11-13 20:21:30 +0530

    Implemented Hero Section Animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useRef } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { SplitText } from "gsap/all";
+import { useMediaQuery } from "react-responsive";
+
 
 const Hero = () => {
+
+  const videoRef = useRef();
+  const videoTimelineRef = useRef();
+
+
+  const isMobile = useMediaQuery({maxWidth:767})
+
   useGSAP(() => {
     const heroSplit = new SplitText(".title", { type: "chars, words" });
     const paragraphSplit = new SplitText(".subtitle", { type: "lines" });
@@ -26,17 +35,37 @@ const Hero = () => {
       delay: 1,
     });
 
-    gsap.timeline({
-      scrollTrigger: {
-        trgigger: "#hero", //isko trigger krna hai
-        start: "top top", // kab karna hai jab homepage hits the top of the screen
-        end: "bottom top", //end krna hai jab bottom of the page hits the top f screen
-        scrub: true, //iss property ka matlab hai ki animation progress will be directly related to scroll which will make it feel natural
-      },
-    })
-    .to('right-leaf',{y:200},0)
-    .to('left-leaf',{y:-200},0)
-    
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#hero", //isko trigger krna hai
+          start: "top top", // kab karna hai jab homepage hits the top of the screen
+          end: "bottom top", //end krna hai jab bottom of the page hits the top f screen
+          scrub: true, //iss property ka matlab hai ki animation progress will be directly related to scroll which will make it feel natural
+        },
+      })
+      .to("right-leaf", { y: 200 }, 0)
+      .to("left-leaf", { y: -200 }, 0);
+
+      const startValue = isMobile ? 'top 50%' :'center 60%';
+      const endValue = isMobile ? '120% top' : 'bottom top'
+
+       let tl= gsap.timeline({
+        scrollTrigger:{
+          trigger:"video",
+          start: startValue,
+          end: endValue,
+          scrub:true,//make the video play on scroll
+          pin:true,
+
+        }
+      })
+
+      videoRef.current.onloadedmetadata = () =>{
+        tl.to(videoRef.current,{
+          currentTime:videoRef.current.duration,
+        })
+      }
   }, []); //this empty array means ye sirf component ke start hone pe hi chalega
 
   return (
@@ -75,6 +104,10 @@ const Hero = () => {
           </div>
         </div>
       </section>
+
+      <div className="video absolute inset-0">
+        <video ref={videoRef} src="/videos/input.mp4" muted playsInline preload="auto" />
+      </div>
     </>
   );
 };
